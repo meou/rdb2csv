c54c64121835c22296536ffaab750589143eda33
shangwu <shangwu@yahoo-inc.com>
2013-08-07 01:54:07 -0700

    0.1

--- rdb2csv/cli.py
+++ rdb2csv/cli.py
@@ -5,19 +5,19 @@ import optparse
 from csv_callback import CsvCallback
 from rdbtools import RdbParser
 
-def main(argv):
+def main():
 
   parser = optparse.OptionParser()
-  parser.add_option('-i', '--input', dest='rdb_file', help="input rdb file")
-  parser.add_option('-o', '--output', dest='csv_file', help = "output csv file")
+  parser.add_option('-i', '--input', dest='rdb_file', help="[required] input rdb file")
+  parser.add_option('-o', '--output', dest='csv_file', help = "[required] output csv file")
   parser.add_option('-c', '--column_delimiter', dest='column_delimiter',
-                    help="column delimiter, default is ctrl-A ('\\001')")
+                    help="[optional] column delimiter, default is ctrl-A ('\\001')")
   parser.add_option('-l', '--line_delimiter', dest='line_delimiter',
-                    help="row delimiter, default is newline ('\\n')")
+                    help="[optional] row delimiter, default is newline ('\\n')")
   parser.add_option('-p', '--pre_key', dest='pre_key',
-                    help="columns before hash key, default is empty")
+                    help="[optional] columns before hash key, default is empty")
   parser.add_option('-P', '--post_key', dest='post_key',
-                    help="columns after hash key, default is 'views clicks'")
+                    help="[optional] columns after hash key, default is 'views clicks'")
 
   parser.set_defaults(column_delimiter='\001')
   parser.set_defaults(line_delimiter='\n')
@@ -37,4 +37,4 @@ def main(argv):
   outf.close()
 
 if __name__ == "__main__":
-  main(sys.argv[1:])
+  main()
--- setup.py
+++ setup.py
@@ -3,11 +3,14 @@ from os import popen
 
 setup(
 	name="rdb2csv",
-	version="0.0.1",
+	version="0.1",
 	url='https://github.com/meou/rdb2csv',
 	author="Shang-Chieh Wu",
 	author_email="pypi@meou.org",
 	packages=["rdb2csv"],
 	license="MIT",
 	long_description=open('README.md').read(),
+	entry_points={
+          'console_scripts':
+              ['rdb2csv = rdb2csv.cli:main']}
 )
